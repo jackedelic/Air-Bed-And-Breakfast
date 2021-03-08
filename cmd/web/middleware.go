567d3ef45989ccabccf344a2d478ae69f16c702a
Jack Wong <jackwong3101@yahoo.com>
2021-03-09 03:13:57 +0800

    Add NoSurf middleware

---
+++ cmd/web/middleware.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/justinas/nosurf"
+)
+
+// WriteToConsole is an http middleware that write to the fmt before calling the next middleware
+func WriteToConsole(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Hit the page")
+		next.ServeHTTP(w, r)
+	})
+}
+
+// NoSurf is an http middleware that attaches cookie to "/"
+func NoSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   false,
+		SameSite: http.SameSiteLaxMode,
+	})
+	return csrfHandler
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,14 +3,20 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/jackedelic/go-overview-trevor-sawler/pkg/config"
 	"github.com/jackedelic/go-overview-trevor-sawler/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// mux := pat.New()
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewMux()
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
