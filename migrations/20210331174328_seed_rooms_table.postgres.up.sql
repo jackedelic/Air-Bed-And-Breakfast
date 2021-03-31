590b5e1ffe55fce22670cac81c307cf97baa4cb3
Jack Wong <jackwong3101@yahoo.com>
2021-04-01 02:25:29 +0800

    Create SeedRoomsTable migration

---
+++ migrations/20210331174328_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms(id, room_name, created_at, updated_at) VALUES 
+("1", "Generals Quarters", "2021-01-01 00:00:00", "2021-01-01 00:00:00"),
+("2", "Major's Room", "2021-03-27 00:00:00", "2021-03-27 00:00:00");
