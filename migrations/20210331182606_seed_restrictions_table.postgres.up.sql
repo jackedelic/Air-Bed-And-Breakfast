7d78ad952a7d31bb5e594bd9c4f8ddda69d574e2
Jack Wong <jackwong3101@yahoo.com>
2021-04-01 02:36:40 +0800

    Create SeedRestrictionsTable migration

---
+++ migrations/20210331182606_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.restrictions(
+	restriction_name, created_at, updated_at)
+	VALUES 
+  ("Reservation", "2020-12-12 00:00:00", "2020-12-12 00:00:00"),
+	("Owner Block", "2020-12-12 00:00:00", "2020-12-12 00:00:00"),
+	("Owner Block", "2020-12-12 00:00:00", "2020-12-12 00:00:00"),
