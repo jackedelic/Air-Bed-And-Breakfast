4488e70da42b2c0c5813e645c35b335d35e4d774
Jack Wong <jackwong3101@yahoo.com>
2021-04-12 17:36:50 +0800

    Implement AdminReservationsCalendar handler to show the Reservation Calendar on the admin site

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -673,7 +673,33 @@ func (m *Repository) AdminUpdateReservation(w http.ResponseWriter, r *http.Reque
 
 // AdminReservationsCalendar renders the reservations calendar page for admin
 func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Request) {
-	render.Template(w, r, "admin-reservations-calendar.page.tmpl", &models.TemplateData{})
+	// Assume that there is no month or year specified
+	now := time.Now()
+	if r.URL.Query().Get("y") != "" {
+		year, _ := strconv.Atoi(r.URL.Query().Get("y"))
+		month, _ := strconv.Atoi(r.URL.Query().Get("m"))
+		now = time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.UTC)
+	}
+	next := now.AddDate(0, 1, 0)
+	last := now.AddDate(0, -1, 0)
+
+	nextMonth := next.Format("01")
+	nextMonthYear := next.Format("2006")
+
+	lastMonth := last.Format("01")
+	lastMonthYear := last.Format("2006")
+
+	stringMap := make(map[string]string)
+	stringMap["this_month"] = now.Format("Jan")
+	stringMap["this_month_year"] = now.Format("2006")
+	stringMap["next_month"] = nextMonth
+	stringMap["next_month_year"] = nextMonthYear
+	stringMap["last_month"] = lastMonth
+	stringMap["last_month_year"] = lastMonthYear
+
+	render.Template(w, r, "admin-reservations-calendar.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
 
 // AdminProcessReservation marks a reservation as processed
